8464f2445af147b587c4ba8a053879634f7f51e6
Rift <35949565+Rift73@users.noreply.github.com>
2026-03-30 04:02:13 +0800

    Add ffmpeg to install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+sudo apt install -y ffmpeg
+
 python3.12 -m venv venv
 source venv/bin/activate
-pip install -U torch torchvision --index-url https://download.pytorch.org/whl/cu128
+pip install -U torch torchvision --index-url https://download.pytorch.org/whl/cu130
 pip install .
 
 echo "traiNNer-redux dependencies installed successfully!"
